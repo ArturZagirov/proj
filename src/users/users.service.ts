2e95da27c16f4a9a2ce146e83f64a1b8b6b91da8
ArturZagirov <artur.zagirov.05@mail.ru>
2025-05-04 17:16:58 +0200

    service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,111 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+
+    async create(createUserDto: CreateUserDto) {
+        
+        const existingUser = await this.prisma.user.findFirst({
+            where: {
+                OR: [
+                    {login: createUserDto.login},
+                    {email: createUserDto.email}
+                ]
+            }
+        })
+        if (existingUser) {
+            throw new ConflictException("ERROR User already registered ")
+        }
+
+        return this.prisma.user.create({
+            data: createUserDto
+        })
+    }
+
+    async findAll() {
+        return this.prisma.user.findMany({
+            select: {
+                id: true,
+                login: true,
+                email: true,
+                age: true,
+                description: true,
+            }
+        })
+    }
+    async findOne(id: number) {
+        const user = await this.prisma.user.findUnique({
+            where: {id: id},
+            select: {
+                id: true,
+                login: true,
+                email: true,
+                age: true,
+                description: true,
+                createdAt: true,
+                updatedAt: true,
+            }
+        })
+
+        if(!user) {
+            throw new NotFoundException("Not found User")
+        }
+
+        return user
+    }
+
+    async remove(id: number) {
+        await this.findOne(id)
+
+        return this.prisma.user.delete({
+            where: {id}
+        })
+    }
+    async login(loginUserDto: LoginUserDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {login: loginUserDto.login}
+            }
+        )
+
+        if(!user || user.password !== loginUserDto.password) {
+            throw new UnauthorizedException("Invalid login or password")
+        }
+    }
+
+    async update(id: number, updateUserDto: UpdateUserDto) {
+
+        await this.findOne(id);
+
+        if (updateUserDto.login || updateUserDto.email) {
+            const existingUser = await this.prisma.user.findFirst({
+                where: {
+                    OR: [
+                        updateUserDto.login ? {login: updateUserDto.login} : {},
+                        updateUserDto.email ? {login: updateUserDto.email} : {},
+                    ],
+                    NOT: {
+                        id,
+                    }
+                }
+            })
+        }
+        return this.prisma.user.update({
+            where: {id},
+            data: updateUserDto,
+            select: {
+                id: true,
+                login: true,
+                email: true,
+                age: true,
+                description: true,
+                createdAt: true,
+                updatedAt: true,
+            }
+        })
+    }
+}
