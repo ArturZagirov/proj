b63a6fd4ab740c3070d76b81b33e2a4f9a13c574
ArturZagirov <artur.zagirov.05@mail.ru>
2025-05-04 17:27:52 +0200

    mainTS cookies + pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 3600000, // 1 час
+      },
+    }),
+  );
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
